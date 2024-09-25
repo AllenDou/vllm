ea1a15da99a10ba6edaf388ef1f13e2cc765020b
zixiao <shunli.dsl@alibaba-inc.com>
2024-09-25 18:13:42 +0800

    update.

---
+++ awq_quantize_qwen25_7b.py
@@ -0,0 +1,19 @@
+from awq import AutoAWQForCausalLM
+from transformers import AutoTokenizer
+
+#model_path = 'lmsys/vicuna-7b-v1.5'
+#quant_path = 'vicuna-7b-v1.5-awq'
+model_path = 'Qwen/Qwen2.5-7B'
+quant_path = 'Qwen2.5-7B-awq'
+quant_config = { "zero_point": True, "q_group_size": 128, "w_bit": 4, "version": "GEMM" }
+
+# Load model
+model = AutoAWQForCausalLM.from_pretrained(model_path)
+tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+# Quantize
+model.quantize(tokenizer, quant_config=quant_config)
+
+# Save quantized model
+model.save_quantized(quant_path)
+tokenizer.save_pretrained(quant_path)
